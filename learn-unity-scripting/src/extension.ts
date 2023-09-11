1c5acfed84cfed4bd30b270bcf5c950422a320e3
Debbie Yuen <deborahyuen2016@berkeley.edu>
2023-09-11 01:54:46 -0700

    unity walkthrough

--- learn-unity-scripting/src/extension.ts
+++ learn-unity-scripting/src/extension.ts
@@ -3,6 +3,7 @@
 import * as vscode from 'vscode';
 import { UnityPanel } from './UnityPanel';
 import { XMLParser } from 'fast-xml-parser';
+import { SidebarProvider } from './SidebarProvider';
 
 const axios = require('axios').default;
 const parser = new XMLParser();
@@ -11,8 +12,8 @@ const parser = new XMLParser();
 // Your extension is activated the very first time the command is executed
 export async function activate(context: vscode.ExtensionContext) {
 
+	// Fetch RSS feed to populate Unity Learn documentation
 	const res = await axios.get('https://rss.app/feeds/CmRSPz9oXjdA1iOb.xml');
-	// const articles = parser.parse(res.data)
 	const articles = parser.parse(res.data).rss.channel.item.map((article: { title: any; description: any; link: any;}) => {
 		return {
 			label: article.title,
@@ -21,17 +22,10 @@ export async function activate(context: vscode.ExtensionContext) {
 		};
 	});
 	console.log(articles);
-	// console.log(parser.parse(res.data));
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "learn-unity-scripting" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
+	// Register Unity Panel to display content from Unity Learn documentation
 	context.subscriptions.push(
-		vscode.commands.registerCommand('learn-unity-scripting.helloWorld', async () => {
+		vscode.commands.registerCommand('learn-unity-scripting.unityLearnDocs', async () => {
 				UnityPanel.createOrShow(context.extensionUri);
 				const article: any = await vscode.window.showQuickPick(articles, {
 					matchOnDetail: true,
@@ -41,11 +35,50 @@ export async function activate(context: vscode.ExtensionContext) {
 					return;
 				}
 				vscode.env.openExternal(article.link);
-			// The code you place here will be executed every time your command is executed
-			// Display a message box to the user
-			// vscode.window.showInformationMessage('Hello World from VSTodo!');
 		})
 	);
+
+	// Register Unity Sidebar
+	const sidebarProvider = new SidebarProvider(context.extensionUri);
+	context.subscriptions.push(
+		vscode.window.registerWebviewViewProvider(
+			"unity-sidebar",
+			sidebarProvider
+		)
+	);
+
+	// Unity Walkthrough Tutorial
+	
+	// Step 1: Create New Unity Project
+	context.subscriptions.push(vscode.commands.registerCommand('learn-unity-scripting.runCommand', async () => {
+		await new Promise(resolve => setTimeout(resolve, 1000));
+		vscode.commands.executeCommand('learn-unity-scripting.runCommand', vscode.Uri.joinPath(context.extensionUri, 'sample-folder'));
+	}));
+
+	// Step 2: Install Unity Extension
+	context.subscriptions.push(vscode.commands.registerCommand('learn-unity-scripting.installunityextension', () => {
+		vscode.window.showInformationMessage('You installed the Unity Extension!');
+	}));
+
+	// Step 3: Install Copilot Extension
+	context.subscriptions.push(vscode.commands.registerCommand('learn-unity-scripting.installcopilotextension', () => {
+		vscode.window.showInformationMessage('You installed the Copilot Extension!');
+	}));
+
+	// Step 4: Create a C# Script in Unity
+	context.subscriptions.push(vscode.commands.registerCommand('learn-unity-scripting.createscript', () => {
+		vscode.window.showInformationMessage('Create a C# Script');
+	}));
+
+	// Step 5: Open Terminal
+	context.subscriptions.push(vscode.commands.registerCommand('learn-unity-scripting.openterminalview', () => {
+		return { openFolder: vscode.Uri.joinPath(context.extensionUri, 'src') };
+	}));
+
+	// Step 6: Open Unity Project in VS Code
+	context.subscriptions.push(vscode.commands.registerCommand('learn-unity-scripting.viewSources', () => {
+		return { openFolder: vscode.Uri.joinPath(context.extensionUri, 'src') };
+	}));
 }
 
 // This method is called when your extension is deactivated
